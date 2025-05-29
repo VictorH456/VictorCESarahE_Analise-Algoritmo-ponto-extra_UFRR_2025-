2cb2b9e3928d50cf6907da582d848c49408ecbe9
Victor Hugo <120677166+VictorH456@users.noreply.github.com>
2025-05-29 10:14:45 -0400

    Adição das entradas e algoritmos

---
+++ algoritmos/insertion_sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern long comparacoes;
+
+void insertion_sort(int *arr, int n) {
+    for (int i = 1; i < n; i++) {
+        int chave = arr[i];
+        int j = i - 1;
+
+        while (j >= 0) {
+            comparacoes++;
+            if (arr[j] > chave) {
+                arr[j + 1] = arr[j];
+                j--;
+            } else {
+                break;
+            }
+        }
+        arr[j + 1] = chave;
+    }
+}
